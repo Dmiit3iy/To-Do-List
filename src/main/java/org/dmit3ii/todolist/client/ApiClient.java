3034197f339271035702298468f605aab1632c5b
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2024-12-28 11:01:02 +0500

    добавил ApiClient

---
+++ src/main/java/org/dmit3ii/todolist/client/ApiClient.java
@@ -0,0 +1,25 @@
+package org.dmit3ii.todolist.client;
+
+
+import org.dmit3ii.todolist.model.Holiday;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ApiClient {
+    @Value("${url.api.path}")
+    private String apiUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping
+    public ResponseEntity<Holiday> getAllHolidays(int year, String countryCode) {
+        return restTemplate.exchange(apiUrl + "/{year}/{countryCode}", HttpMethod.GET, null,
+                new ParameterizedTypeReference<>() {}, year, countryCode);
+    }
+}
