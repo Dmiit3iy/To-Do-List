da971fa7926e96c6e1683ea81b070073aaef1a8d
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2024-12-28 10:05:52 +0500

    initial commit (plus:add Enum TypeOfHoliday and Holiday.class)

---
+++ src/main/java/org/dmit3ii/todolist/model/Holiday.java
@@ -0,0 +1,24 @@
+package org.dmit3ii.todolist.model;
+
+import java.lang.reflect.Type;
+
+public class Holiday {
+
+    private String date;
+
+    private String localName;
+
+    private String name;
+
+    private String countryCode;
+
+    private Boolean fixed;
+
+    private Boolean global;
+
+    private Object counties;
+
+    private Integer launchYear;
+
+    private List<TypeOfHoliday> types;
+}
---
+++ src/main/java/org/dmit3ii/todolist/model/TypeOfHoliday.java
@@ -0,0 +1,10 @@
+package org.dmit3ii.todolist.model;
+
+public enum TypeOfHoliday {
+    PUBLIC,
+    BANK,
+    SCHOOL,
+    AUTHORITIES,
+    OPTIONAL,
+    OBSERVANCE
+}
